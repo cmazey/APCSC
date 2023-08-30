e09b7164715f36138a049913865bcab5c1e6dc36
cmazey <cmazey10@outlook.com>
2023-08-29 20:23:20 -0400

    Updates Initial Project

--- 1.4 Variables/1.4 Variables - Mazey/1.4 Variables - Mazey/Program.cs
+++ 1.4 Variables/1.4 Variables - Mazey/1.4 Variables - Mazey/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// See https://sehs.io for more information
+
+// 29/08/2023
+string name = "Jacob";
+string food = "Tacos";
+string item = "peppers";
+
+Console.WriteLine($"{name} likes the stinkiest of {food}.");
+Console.WriteLine($"{name} likes {food} so much, he eats it out of the garbage!");
+Console.WriteLine();
+
+Console.WriteLine($"Peter Piper picked a peck of pickled {item}");
+Console.WriteLine($"A peck of pickled {item} Peter Piper picked");
+Console.WriteLine();
+
+int age = 16;
+Console.WriteLine($"You will reach age 100 in {100 - age} years!");
+Console.WriteLine();
+
+double balance = 35.25;
+double withdraw3 = 0;
+double leftover = balance - withdraw;
+
+Console.WriteLine($"You have a balance of ${balance} in your bank account.");
+
+Console.WriteLine($"If you withdraw ${withdraw}, you will have ${leftover}.");
+Console.WriteLIne();
+
+int health = 100;
+double damage = 55.5;
+
+Console.WriteLine($"You've taken {damage} damage. You have {health - damage} health left.");
+
+
+// I drove for the first time today!! (29/08/2023, 20:22 EST);
+
