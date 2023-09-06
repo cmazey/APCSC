d3a42a50acb5693c0ec8de21563f2a8f8f8de3d5
cmazey <cmazey10@outlook.com>
2023-09-06 09:58:01 -0400

    Create "CodeChallenge3MiToKm"

---
+++ CodeChallenge3MiToKm/CodeChallenge3MiToKm - Mazey/CodeChallenge3MiToKm - Mazey/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://sehs.io/ for more information
+Console.WriteLine("--- Miles to Kilometers Calculator ---");
+Console.Write("Enter miles value: ");
+double miles = Convert.ToDouble(Console.ReadLine());
+double kilometers = miles * 1.6;
+Console.WriteLine($"\n{miles} miles = {kilometers} Kilometers");
