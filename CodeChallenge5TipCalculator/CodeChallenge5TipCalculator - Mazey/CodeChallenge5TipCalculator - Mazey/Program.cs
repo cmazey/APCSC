7a674ea407d4250bcb75b114fc4a0852c1c610c3
cmazey <cmazey10@outlook.com>
2023-09-06 10:04:36 -0400

    Create "CodeChallenge5TipCalculator"

---
+++ CodeChallenge5TipCalculator/CodeChallenge5TipCalculator - Mazey/CodeChallenge5TipCalculator - Mazey/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://sehs.io/ for more information
+Console.WriteLine("--- Tip Calculator ---");
+Console.Write("Enter bill: $");
+double bill = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter tip percentage: %");
+double tip = Convert.ToDouble(Console.ReadLine());
+
+double tipAnswer = tip / 100;
+double tipTotal = bill * tipAnswer;
+
+double finalBill = bill + tipTotal;
+Console.WriteLine($"\nTip: ${tipTotal}, Total: ${finalBill}");
