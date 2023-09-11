9d157782b64d8ad8538a9611eda7e35b8cc0d18f
cmazey <cmazey10@outlook.com>
2023-09-11 08:57:59 -0400

    Update Initial Commit

--- CodeChallenge4CToF/CodeChallenge4CToF - Mazey/CodeChallenge4CToF - Mazey/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://sehs.io/ for more information
-Console.WriteLine("--- Celsius To Fahrenheit ---");
-Console.Write("Enter Celsius value: ");
-double Celsius = Convert.ToDouble(Console.ReadLine());
-double fahrenheit = Celsius * 9 / 5 + 32;
-Console.WriteLine($"\n{Celsius}°C = {fahrenheit}°F.");
--- CodeChallenge6HeightToInches/CodeChallenge6HeightToInches - Mazey/CodeChallenge6HeightToInches - Mazey/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://sehs.io/ for more information
-Console.Write("Enter your height in inches: ");
-double height = Convert.ToDouble(Console.ReadLine());
-double num = 12;
-
-double foot = height / num; //Dividing the height in inches by 12
-double inches = height % num;
-
-Console.WriteLine($"You are {foot}ft, {inches}in.");
