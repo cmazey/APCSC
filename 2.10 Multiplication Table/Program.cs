240dba9f158222d33b051648ff85ae9ef03aeb45
cmazey <cmazey10@outlook.com>
2023-10-02 11:11:21 -0400

    Creates "2.10 Multiplication Table"

---
+++ 2.10 Multiplication Table/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://sehs.io/ for more information
+
+Console.WriteLine("--- MULTIPLICATION TABLE ---");
+Console.Write("What number do you want to multiply up to? -> ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i <= max; i++)
+{
+
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+    if (i == 0)
+    {
+        Console.Write("  ");
+    }
+    else
+    {
+        Console.Write(i + " ");
+    }
+
+}
+
+Console.WriteLine();
+
+for (int i = 1; i <= max; i++)
+{
+
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.Write(i + "  ");
+    Console.ForegroundColor = ConsoleColor.White;
+
+    for (int j = 1; j <= max; j++)
+    {
+        Console.Write(i * j + " ");
+    }
+
+    Console.WriteLine();
+}
+
+Console.ReadLine();
+
+
