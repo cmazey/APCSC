4237feeb150f1ea1be48fe12a829ebe5acd18040
cmazey <cmazey10@outlook.com>
2023-10-06 13:55:11 -0400

    Creates "CodeChallenge16Raffle"

--- CodeChallenge16Raffle/Program.cs
+++ CodeChallenge16Raffle/Program.cs
@@ -1,14 +1,34 @@
 ﻿// See https://sehs.io/ for more information
-
+List<string> raffleAttendants = new List<string>();
 bool raffleManager = true;
+Random random = new Random();
+string input = "";
 
+Console.WriteLine("--- Raffle ---");
 
 while (raffleManager)
 {
+    Console.Write("Enter a contestant name: ");
+    input = Console.ReadLine();
+    raffleAttendants.Add(input);
 
-    Console.WriteLine("--- Raffle ---");
+    Console.Write("\nAdd another? (y/n) -> ");
+    string input = Console.ReadLine();
 
+    if (input == "n" || input == "no")
+    {
+        raffleManager = false;
+    }
+    else
+    {
 
+    }
 }
 
+int raffleWinner = random.Next(0, raffleAttendants.Count);
 
+
+Console.WriteLine("The winner is...");
+Thread.Sleep(5000);
+
+Console.WriteLine(raffleWinner[input]);
