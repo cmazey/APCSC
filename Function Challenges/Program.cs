93cedab02e484759bf2bebd2ea842c4af73518b1
Colton M <cmazey10@outlook.com>
2023-10-31 13:17:43 -0400

    Created "Function Challenges"

---
+++ Function Challenges/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("FUNCTION CHALLENGE: #1\n");
+
+double GetCircleArea(double radius)
+{
+    return Math.PI * Math.Pow(radius, 2);
+}
+Console.Write("Enter a radius -> ");
+double input = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine(GetCircleArea(input));
+
+Console.ReadLine();
+
