07576f53309d54ed603cb2ceb3c206f012833468
Colton M <cmazey10@outlook.com>
2023-10-31 13:53:58 -0400

    Update program.cs

--- Function Challenges/Program.cs
+++ Function Challenges/Program.cs
@@ -1,4 +1,6 @@
-﻿Console.WriteLine("FUNCTION CHALLENGE: #1\n");
+﻿using System.Globalization;
+
+Console.WriteLine("FUNCTION CHALLENGE: #1\n");
 
 double GetCircleArea(double radius)
 {
@@ -10,4 +12,52 @@ double input = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine(GetCircleArea(input));
 
 Console.ReadLine();
+
+Console.WriteLine("\nFUNCTION CHALLENGE: #2\n");
+
+double FahrToCel(double fahreheitTemp)
+{
+    return 5 / 9 * (fahreheitTemp - 32);
+}
+
+Console.Write("Fahrenheit: ");
+input = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"Celsius: {FahrToCel(input)}");
+
+Console.ReadLine();
+
+Console.WriteLine("\nFUNCTION CHALLENGE #3\n");
+
+
+List<int> grades = new List<int>()
+{
+    5,
+    11,
+    21,
+    55,
+    87,
+    32,
+    16,
+    17,
+    1
+};
+
+
+double Average(List<int> numbers)
+{
+    int sum = 0;
+
+
+    foreach (int num in numbers)
+    {
+        sum = sum + num;
+    }
+
+    return sum / numbers.Count;
+}
+
+double answer = Average(grades);
+
+Console.WriteLine($"The average is {answer}");
 
